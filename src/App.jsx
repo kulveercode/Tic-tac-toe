c555e258de67b1bef6a5f8b7184a14de4d3f4230
Kulveer Singh <kulveer.coder@gmail.com>
2023-10-26 22:45:32 +0530

    board marks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,30 +14,37 @@ function App() {
 }
 
 function Board(){
+  const [marks, setMarks] = useState([0,0,0,0,0,0,0,0,0]);
+  const changeMark = (i)=>{
+    const m =[...marks];
+    m[i] = 1;
+    setMarks(m);
+  }
+
   return (
     <div className="Board">
       <div>
-        <Block />
-        <Block />
-        <Block />
+        <Block mark={marks[0]} position={0} changeMark={changeMark} />
+        <Block mark={marks[1]} position={1} changeMark={changeMark} />
+        <Block mark={marks[2]} position={2} changeMark={changeMark} />
       </div>
       <div>
-        <Block />
-        <Block />
-        <Block />
+        <Block mark={marks[3]} position={3} changeMark={changeMark} />
+        <Block mark={marks[4]} position={4} changeMark={changeMark} />
+        <Block mark={marks[5]} position={5} changeMark={changeMark} />
       </div>
       <div>
-        <Block />
-        <Block />
-        <Block />
+        <Block mark={marks[6]} position={6} changeMark={changeMark} />
+        <Block mark={marks[7]} position={7} changeMark={changeMark} />
+        <Block mark={marks[8]} position={8} changeMark={changeMark} />
       </div>
     </div>
   )
 }
 
-function Block({mark, changeMark}){
-  return <div className={`Block ${mark}`} onClick={e=>changeMark()}>
-
+function Block({mark, changeMark, position}) {
+  return <div className= {`Block mark${mark}`} 
+          onClick={e=> changeMark(position)}>
   </div>
 }
 
